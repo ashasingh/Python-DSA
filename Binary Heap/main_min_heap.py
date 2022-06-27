125791f093f6f45e9ca704811e6a6e37fd1a4cd5
ashasingh <asha.singh7558@gmail.com>
2022-06-27 17:07:00 +0530

    Create main_min_heap.py

---
+++ Binary Heap/main_min_heap.py
@@ -0,0 +1,67 @@
+# Min-Heap data structure in Python
+
+# call min-heap heapify
+def heapify(arr, n, i):
+    smallest = i
+    l = 2 * i + 1
+    r = 2 * i + 2 
+    
+    if l < n and arr[i] >= arr[l]:
+        smallest = l
+
+    if r < n and arr[smallest] >= arr[r]:
+        smallest = r
+    
+    if smallest != i:
+        arr[i],arr[smallest] = arr[smallest],arr[i]
+        heapify(arr, n, smallest)
+
+# add elemnt to tree at the end and call heapify
+def insert(array, newNum):
+    size = len(array)
+    if size == 0:
+        array.append(newNum)
+    else:
+        array.append(newNum);
+        for i in range((size//2), -1, -1):
+            heapify(array, size, i)
+
+# delete element and call heapify
+def deleteNode(array, num):
+    size = len(array)
+    i = 0
+    for i in range(0, size):
+        if num == array[i]:
+            break
+        
+    array[i], array[size-1] = array[size-1], array[i]
+
+    array.remove(num)
+    
+    for i in range((len(array)//2), -1, -1):
+        heapify(array, len(array), i)
+
+## create empty array  
+arr = []
+
+# Insertion in Max-Heap
+insert(arr, 2)
+insert(arr, 4)
+insert(arr, 1)
+insert(arr, 5)
+insert(arr, 3)
+print ("Min-Heap array: " + str(arr))
+
+# delete item from array
+deleteNode(arr, 4)
+print("After deleting an element: " + str(arr))
+
+# get the min element
+print("Peek of minimum element: " + str(arr[0]))
+
+# Extract min - get the min element from array and delete from the array
+print("Extract Min: " + str(arr[0]))
+deleteNode(arr, arr[0])
+print ("Min-Heap array: " + str(arr))
+
+
