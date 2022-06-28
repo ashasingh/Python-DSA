a51f6c9d4184bc13344998e7fc0756bdad486404
ashasingh <asha.singh7558@gmail.com>
2022-06-28 15:43:19 +0530

    Update create_BT_using_binarytree.py

--- Trees/create_BT_using_binarytree.py
+++ Trees/create_BT_using_binarytree.py
@@ -42,4 +42,26 @@ print('\nList from binary tree :',
 #Build a random binary tree:
 #tree() generates a random binary tree and returns its root node.
 
+from binarytree import tree
+
+# Create a random binary
+# tree of any height
+root = tree()
+print("Binary tree of any height :")
+print(root)
+
+# Create a random binary
+# tree of given height
+root2 = tree(height = 2)
+print("Binary tree of given height :")
+print(root2)
+
+# Create a random perfect
+# binary tree of given height
+root3 = tree(height = 2,
+			is_perfect = True)
+print("Perfect binary tree of given height :")
+print(root3)
+
+
 ## this module is further used to create heaps and bst
