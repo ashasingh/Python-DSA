1acea573694d70faf991e15ab2b4ec6dba66921b
ashasingh <asha.singh7558@gmail.com>
2022-06-28 18:24:43 +0530

    Create create_balanced_binary_tree.py

---
+++ Trees/create_balanced_binary_tree.py
@@ -0,0 +1,53 @@
+# Checking if a binary tree is height balanced in Python
+
+# create new node
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+# class for height
+class Height:
+    def __init__(self):
+        self.height = 0
+
+# check if balanced or not
+def isHeightBalanced(root, height):
+
+    left_height = Height()
+    right_height = Height()
+
+    if root is None:
+        return True
+
+    l = isHeightBalanced(root.left, left_height)
+    r = isHeightBalanced(root.right, right_height)
+
+    height.height = max(left_height.height, right_height.height) + 1
+
+    if abs(left_height.height - right_height.height) <= 1:
+        return l and r
+
+    return False
+
+
+height = Height()
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+if isHeightBalanced(root, height):
+    print('The tree is balanced')
+else:
+    print('The tree is not balanced')
+
+# add new node to the tree and check
+root.left.left.right = Node(4)
+if isHeightBalanced(root, height):
+    print('The tree is balanced')
+else:
+    print('The tree is not balanced')
